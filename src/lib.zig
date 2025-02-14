50c210b40d49979c1a32aa78663143edfd2339fc
Andre Toerien <andre.toerien8@gmail.com>
2025-02-14 14:09:04 +0200

    init: initial commit

---
+++ src/lib.zig
@@ -0,0 +1,63 @@
+const builtin = @import("builtin");
+const std = @import("std");
+
+pub const Color = union(enum) {
+    esc,
+    none,
+    end,
+    bold,
+    invert,
+    color: struct { fg: []const u8, bg: []const u8 },
+    color_attr: struct { fg: []const u8, attr: []const u8, bg: []const u8 },
+};
+
+pub fn color(out: std.io.AnyWriter, col: Color) !void {
+    switch (col) {
+        .esc => { // esc
+            _ = try out.write("");
+        },
+        .none => { // -, none
+            _ = try out.write("#[default]");
+        },
+        .end => { // --
+            _ = try out.write("#[default] ");
+        },
+        .bold => { // bold
+            _ = try out.write("#[default]#[fg=bold]");
+        },
+        .invert => { // invert
+            _ = try out.write("#[default]#[reverse]");
+        },
+        .color => |val| { // {back} {fore}
+            _ = try out.print("#[default]#[fg={s},bg={s}]", .{ val.fg, val.bg });
+        },
+        .color_attr => |val| { // {attr} {back} {fore}
+            _ = try out.print("#[default]#[fg={s},{s},bg={s}]", .{ val.fg, val.attr, val.bg });
+        },
+    }
+}
+
+const errno_map = errno_map: {
+    var max_value = 0;
+    for (std.enums.values(std.c.E)) |v|
+        max_value = @max(max_value, @intFromEnum(v));
+
+    var map: [max_value + 1]anyerror = undefined;
+    @memset(&map, error.Unexpected);
+    for (std.enums.values(std.c.E)) |v|
+        map[@intFromEnum(v)] = @field(anyerror, "E" ++ @tagName(v));
+
+    break :errno_map map;
+};
+
+pub fn errnoToZigErr(err: anytype) anyerror {
+    const num = if (@typeInfo(@TypeOf(err)) == .Enum)
+        @intFromEnum(err)
+    else
+        err;
+
+    if (num > 0 and num < errno_map.len)
+        return errno_map[@intCast(num)];
+
+    return error.Unexpected;
+}
---
+++ src/main.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+const lib = @import("lib.zig");
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len < 2) {
+        //
+    } else if (std.mem.eql(u8, args[1], "left")) {
+        //
+    } else if (std.mem.eql(u8, args[1], "right")) {
+        //
+    }
+    try bw.flush();
+}
