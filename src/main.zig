2573681539d49763b96321828cf182246e8117df
Andre Toerien <andre.toerien8@gmail.com>
2025-03-19 10:40:45 +0200

    feat: specify modules on cmdline

--- src/main.zig
+++ src/main.zig
@@ -2,19 +2,32 @@ const std = @import("std");
 
 const lib = @import("lib.zig");
 
-const cpu_count = @import("cpu_count.zig");
-const cpu_freq = @import("cpu_freq.zig");
-const disk = @import("disk.zig");
-const disk_io = @import("disk_io.zig");
-const hostname = @import("hostname.zig");
-const load_average = @import("load_average.zig");
-const logo = @import("logo.zig");
-const memory = @import("memory.zig");
-const network = @import("network.zig");
-const processes = @import("processes.zig");
-const swap = @import("swap.zig");
-const uptime = @import("uptime.zig");
-const whoami = @import("whoami.zig");
+const modules = struct {
+    pub const cpu_count = @import("cpu_count.zig");
+    pub const cpu_freq = @import("cpu_freq.zig");
+    pub const disk = @import("disk.zig");
+    pub const disk_io = @import("disk_io.zig");
+    pub const hostname = @import("hostname.zig");
+    pub const load_average = @import("load_average.zig");
+    pub const logo = @import("logo.zig");
+    pub const memory = @import("memory.zig");
+    pub const network = @import("network.zig");
+    pub const processes = @import("processes.zig");
+    pub const swap = @import("swap.zig");
+    pub const uptime = @import("uptime.zig");
+    pub const whoami = @import("whoami.zig");
+};
+
+fn runModule(mod: []const u8, ctx: *const lib.Context) !void {
+    inline for (@typeInfo(modules).@"struct".decls) |decl| {
+        if (std.mem.eql(u8, mod, decl.name)) {
+            const module = @field(modules, decl.name);
+            try module.run(ctx);
+            return;
+        }
+    }
+    return error.InvalidModule;
+}
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -37,23 +50,8 @@ pub fn main() !void {
         .cacheDir = cacheDir,
     };
 
-    if (args.len < 2) {
-        //
-    } else if (std.mem.eql(u8, args[1], "left")) {
-        try logo.run(&ctx);
-        try whoami.run(&ctx);
-        try hostname.run(&ctx);
-    } else if (std.mem.eql(u8, args[1], "right")) {
-        try load_average.run(&ctx);
-        try processes.run(&ctx);
-        try cpu_count.run(&ctx);
-        // try cpu_freq.run(&ctx);
-        try memory.run(&ctx);
-        try swap.run(&ctx);
-        try disk.run(&ctx);
-        try disk_io.run(&ctx);
-        try network.run(&ctx);
-        try uptime.run(&ctx);
+    for (args[1..]) |arg| {
+        try runModule(arg, &ctx);
     }
     try bw.flush();
 }
